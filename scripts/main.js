c23bf8cb42f2919d1784fd5d0ec2afe71d65c668
JuanGiacalone <juanpablogiacalone8@gmail.com>
2020-08-14 23:20:13 -0300

    added modal501

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,18 @@
-function myFunction(x) {
-    x.classList.toggle("change");
+//MODAL
+
+  // Obtiene el modal
+  var modal = document.getElementById("miModal");
+
+  // OnClick modal501() muestra el modal
+function modal501(){
+ 
+  modal.style.display = "block";
+
+  // Cerrar cuando el usuario clickea afuera del modal
+  window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+    }
   }
+  }
+//MODAL
